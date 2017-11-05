9d121f5c504e23707be9579e6d80eb9466ac5613
Frederick Kellison-Linn <fjk2119@columbia.edu>
2017-11-05 12:55:28 -0500

    add tester

---
+++ exchange/g1/Tester.java
@@ -0,0 +1,72 @@
+package exchange.g1;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import exchange.g1.Blossom;
+import exchange.sim.Sock;
+import exchange.sim.Multiset;
+
+public class Tester {
+    public static void main(String[] args) {
+        for (int n = 10; n <= 100; n += 10) {
+            long totalRuntime = 0;
+            for (int i = 0; i < 25; i++) {
+                totalRuntime += testRuntime(n);
+            }
+            double averageRuntime = ((double)totalRuntime) / 10;
+            System.out.println(n + "\t" + averageAverageDistance + "\t" + averageRuntime);
+        }
+    }
+    
+    public static long testRuntime(int n) {
+        ArrayList<Sock> socks = new ArrayList<Sock>();
+        Random random = new Random();
+        for (int i = 0; i < 2 * n; ++i) {
+            Sock sock = new Sock(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            socks.add(sock);
+        }
+        long start = System.currentTimeMillis();
+        pair(socks);
+        long elapsed = System.currentTimeMillis() - start;
+        return elapsed;
+    }
+    
+    public static double testAverageDistance(int n) {
+        ArrayList<Sock> socks = new ArrayList<Sock>();
+        Random random = new Random();
+        for (int i = 0; i < 2 * n; ++i) {
+            Sock sock = new Sock(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            socks.add(sock);
+        }
+        pair(socks);
+        double totalDistance = 0.0;
+        for (int i = 0; i < 2 * n - 1; i += 2) {
+            totalDistance += socks.get(i).distance(socks.get(i + 1));
+        }
+        return totalDistance / n;
+    }
+    
+    public static void pair(ArrayList<Sock> socks) {
+        Sock[] sockArray = socks.toArray(new Sock[socks.size()]);
+        int[] match = new Blossom(getCostMatrix(sockArray), true).maxWeightMatching();
+        socks.clear();
+        for (int i = 0; i < match.length; i++) {
+            if (match[i] < i) continue;
+            socks.add(sockArray[i]);
+            socks.add(sockArray[match[i]]);
+        }
+    }
+    
+    private static float[][] getCostMatrix(Sock[] sockArray) {
+        float[][] matrix = new float[sockArray.length * (sockArray.length - 1)/2][3];
+        int idx = 0;
+        for (int i = 0; i < sockArray.length; i++) {
+            for (int j=i+1; j< sockArray.length; j++) {
+                matrix[idx] = new float[]{i, j, (float)(-sockArray[i].distance(sockArray[j]))};
+                idx ++;
+            }
+        }
+        return matrix;
+    }
+}
